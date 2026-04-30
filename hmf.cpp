4f9e21a4baab8082785cf46f47044dc033a8978b
Murilo Ijanc <murilo@ijanc.org>
2026-04-30 11:52:00 -0300

    Use murilo@ijanc.org in hmf.cpp license header

--- hmf.cpp
+++ hmf.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025-2026 Murilo Ijanc' <ijancmurilo@gmail.com>
+ * Copyright (c) 2025-2026 Murilo Ijanc' <murilo@ijanc.org>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
